bd1d909821cf0a9829338758c30454953b10b9d2
Ilya Belyakov <ilyabelyakov@e6:1e:a9:87:e2:9b.home>
2025-07-04 21:28:06 -0400

    Add redo with U

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -18,6 +18,9 @@ vim.keymap.set('n', '<C-a>', 'ggVG', opts)
 -- Undo
 vim.keymap.set({ 'n', 'v', 'i' }, '<C-z>', '<ESC>u', opts)
 
+-- Redo
+vim.keymap.set({ 'n', 'v', 'i' }, 'U', '<C-r>', opts)
+
 -- Go to Beginning/End of the line
 vim.keymap.set({ 'n', 'v' }, 'H', '^', opts)
 vim.keymap.set({ 'n', 'v' }, 'L', '$', opts)
