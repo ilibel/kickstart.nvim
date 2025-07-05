4cd783c4ecb4fe5a1829c13ce7c47e2f90cf41d7
Ilya Belyakov <ilyabelyakov@e6:1e:a9:87:e2:9b.home>
2025-07-04 21:34:02 -0400

    Add Kotlin lsp

--- init.lua
+++ init.lua
@@ -676,6 +676,7 @@ require('lazy').setup({
         -- ts_ls = {},
         --
         -- ruby_lsp = {},
+        kotlin_language_server = {},
 
         lua_ls = {
           -- cmd = { ... },
@@ -1029,7 +1030,22 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'elixir', 'javascript', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'elixir',
+        'javascript',
+        'vim',
+        'vimdoc',
+        'kotlin',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
