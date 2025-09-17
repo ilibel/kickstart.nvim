4a84db0271b032348e11072dd5c5bbd36039a079
Ilya Belyakov <ilyabelyakov@b6:af:43:18:54:94.home>
2025-09-16 22:20:39 -0400

    Add nvim-tree plugin

---
+++ lua/custom/plugins/nvim-tree.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    'nvim-tree/nvim-tree.lua',
+    config = function()
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+
+      local function my_on_attach(bufnr)
+        local api = require 'nvim-tree.api'
+
+        local function opts(desc)
+          return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        -- copy default mappings here from defaults in next section
+        vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts 'Open')
+        vim.keymap.set('n', 'l', api.node.open.edit, opts 'Open')
+      end
+
+      require('nvim-tree').setup {
+        on_attach = my_on_attach,
+        hijack_cursor = true,
+        sort = {
+          sorter = 'case_sensitive',
+        },
+        view = {
+          width = 30,
+        },
+        renderer = {
+          group_empty = true,
+        },
+        filters = {
+          dotfiles = false,
+        },
+      }
+    end,
+  },
+}
